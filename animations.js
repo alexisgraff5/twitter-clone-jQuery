a4935bac62fe8b97743336bd72434e33390654c4
alexisgraff5 <lex.graff@gmail.com>
2016-10-21 16:09:56 -0600

    tweet button and char count are hidden until click in box, char count turns red when 10 chars are left, tweet button disables when char count is over max char

--- animations.js
+++ animations.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
+  // STEP 1 on index.html
   // STEP 2
-  $('textarea.tweet-compose').on('click', function(){
+  $('.tweet-compose').on('click', function(){
     $(this).animate({height: '5em' }, 500);
+    $('#tweet-controls').css("display", "block");
   });
   /* another way to make textarea double in size.
   $('textarea.tweet-compose').focus(function(){
     $(this).animate({height: '5em' }, 500);
   });*/
-  
+
+  // STEP 3 & 4
+  maxCharacters = 140;
+  $('#char-count').text(maxCharacters);
+  $('textarea').bind('keyup keydown', function(){
+    var characters = $('textarea').val().length;
+    if (characters >= (maxCharacters - 10)) {
+      $('#char-count').css('color', 'red');
+    } else {
+      $('#char-count').css('color', '#999');
+    }
+    $('#char-count').text(maxCharacters - characters);
+
+    // STEP 5
+    if (characters > maxCharacters){
+      //$('#tweet-submit').addClass('disabled');
+      $('#tweet-submit').prop('disabled', true);
+    } else {
+      //$('#tweet-submit').removeClass('disabled').;
+      $('#tweet-submit').prop('disabled', false);
+    }
+  });
 });
